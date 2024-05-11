133574bf07ebb799d5224d19b65719e04e4b9d2c
Asrifa-hassan <151858628+Asrifa-hassan@users.noreply.github.com>
2024-05-11 20:29:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('toggle-btn')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+toggleBtn = onclick=function(){
+dropDownMenu.classList.toggle('open')
+const isOpen = dropDownMenu.classList.contains('open')
+}
